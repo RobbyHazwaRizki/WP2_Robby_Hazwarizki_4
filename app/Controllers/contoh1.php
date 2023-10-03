0148d542aa4e160c9f2c62bff4b8723f2ef9859d
RobbyHazwaRizki <robbyhazwarizky27@gmail.com>
2023-10-03 21:35:38 +0700

    menambahkan pustaka-booking ke repository

---
+++ app/Controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo " Nama saya Robby HazwaRizki saya tinggal di kampung karang tengah, olahraga yang saya sukai adalah basket ";
+    }
+}
